98e67cd4d51da2411e5be5d57664cbcd5b9386bb
James Soth <97543733+Blusions@users.noreply.github.com>
2025-10-11 15:52:33 -0700

    game state comments

--- Assets/_Scripts/GameState.cs
+++ Assets/_Scripts/GameState.cs
@@ -6,14 +6,24 @@ public class GameState : MonoBehaviour
     public List<Gestures> gestures;
     public CardManager cardManager;
 
-    public void Start() {
+    public void Start()
+    {
         AddCard(Gestures.Rock);
         AddCard(Gestures.Paper);
         AddCard(Gestures.Scissors);
     }
 
-    public void AddCard(Gestures gesture) {
+    public void AddCard(Gestures gesture)
+    {
         gestures.Add(gesture);
         cardManager.AddCard(gesture);
     }
 }
+
+
+//know what hand player has played
+//outcome of the round 
+//know which 'level' the player is on - based on which enemy prefab is active
+//access to list of gestures per 'level'
+//knows the total number of hands the player has currently, is hands == 0 game over
+//public gameobject of end screen ui
