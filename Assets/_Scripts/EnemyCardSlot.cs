f9e2034b4bbfe33986d9a294f2d2a06107f1bc9b
ryan soohoo <ryansoohoo@gmail.com>
2025-10-12 17:34:41 -0700

    enemy card naming

--- Assets/_Scripts/EnemyCardSlot.cs
+++ Assets/_Scripts/EnemyCardSlot.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class EnemyCardSlot : MonoBehaviour
 {
     public RawImage image;
     public EGestures gesture;
-
+    public TextMeshProUGUI leftText;
+    public TextMeshProUGUI rightText;
     public void ChangeImage(EGestures gesture) {
         image.texture = CardManager.Instance.gestureSprites.Get(gesture).texture;
+        leftText.text = gesture.ToString();
+        rightText.text = gesture.ToString();
     }
 }
